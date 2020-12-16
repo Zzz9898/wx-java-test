bff35667ba8386584dbbd66cac97fe389f57b3be
zhangjw <1617386850@qq.com>
2020-12-16 20:02:33 +0800

    first commit

---
+++ src/main/java/wx/java/test/WxJavaApplication.java
@@ -0,0 +1,11 @@
+package wx.java.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxJavaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WxJavaApplication.class);
+    }
+}
